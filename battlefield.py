fc6997ffdf92ff70618cfc12f25db21d4aecb2ee
Shane Tanner <shanetanner03@gmail.com>
2022-09-26 11:36:43 -0500

    Updated Battlefield class

---
+++ battlefield.py
@@ -0,0 +1,16 @@
+class Battlefield:
+    def __init__(self):
+        self.robot = Robot
+        self.dinosaur = Dinosaur
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        print("Welcome to the battle between Robot and Dinosaur!\nWho will be VICTORIOUS?")
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
