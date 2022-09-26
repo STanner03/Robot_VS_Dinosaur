8a705da65f55cf36aa145ee6d4f24d9d78c3cccf
Shane Tanner <shanetanner03@gmail.com>
2022-09-26 11:29:29 -0500

    Updated Robot class

---
+++ robot.py
@@ -0,0 +1,8 @@
+class Robot:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.active_weapon = Weapon
+
+    def attack(self, dinosaur):
+        pass
